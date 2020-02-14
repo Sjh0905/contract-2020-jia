8e8a3dc8ffb83ff06513dfaa1fae456146451ff2
sunjiahui <2570167180@qq.com>
2020-02-14 13:18:39 +0800

    活动，拼团国际化变量添加

--- src/components/vue/DetailsOfTheGroup.vue
+++ src/components/vue/DetailsOfTheGroup.vue
@@ -1,4 +1,4 @@
 <template src="../html/DetailsOfTheGroup.html"></template>
 <style scoped src="../css/DetailsOfTheGroup.css"></style>
 <script src="../js/DetailsOfTheGroup.js"></script>
-<i18n src="../lang/DetailsOfTheGroup.json"></i18n>
+<i18n src="../lang/JoinAGroup.json"></i18n>
