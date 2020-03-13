b08d753c19ac67c00e32c4f433f7b448c13ea432
lcc <1398750946@qq.com>
2020-03-13 15:36:22 +0800

    创建锁仓记录组件

---
+++ src/components/vue/lockingRecord.vue
@@ -0,0 +1,4 @@
+<template src="../html/lockingRecord.html"></template>
+<style scoped src="../css/lockingRecord.css"></style>
+<script src="../js/lockingRecord.js"></script>
+<i18n src="../lang/lockingRecord.json"></i18n>
