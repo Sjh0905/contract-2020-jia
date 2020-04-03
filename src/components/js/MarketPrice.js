166a465f5a34954046e75b68681d6f06edc6c154
lcc <1398750946@qq.com>
2020-04-03 17:32:05 +0800

    cookie存储单条路径币种信息

--- src/components/js/MarketPrice.js
+++ src/components/js/MarketPrice.js
@@ -447,10 +447,11 @@ root.methods.slectSymbol = function (symbol, item) {
   let user_id = this.$store.state.authMessage.userId;
   let user_id_symbol = user_id + '-' + symbol;
   // if(!this.isLogin){
-  !user_id && this.$cookies.set('unlogin_user_symbol_cookie', symbol, 60 * 60 * 24);
+  !user_id && this.$cookies.set('unlogin_user_symbol_cookie', symbol, 60 * 60 * 24 * 30,"/");
   // this.$store.commit('SET_SYMBOL', symbol);
   // }
-  !!user_id && this.$cookies.set('user_symbol_cookie', user_id_symbol, 60 * 60 * 24);
+  // !!user_id && this.$cookies.set('user_symbol_cookie', user_id_symbol, 60 * 60 * 24);
+  !!user_id && this.$cookies.set('user_symbol_cookie', user_id_symbol, 60 * 60 * 24 * 30,"/");
   this.$store.commit('SET_SYMBOL', symbol);
   // 清空委托列表
   this.$store.commit('GET_OPEN_ORDER', []);
