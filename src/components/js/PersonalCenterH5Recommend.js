6d52aa001e0568a2a213e347914513d58b8382b6
lcc <1398750946@qq.com>
2020-04-20 18:28:12 +0800

    H5 个人邀请bug修复

--- src/components/js/PersonalCenterH5Recommend.js
+++ src/components/js/PersonalCenterH5Recommend.js
@@ -97,20 +97,27 @@ root.data = function () {
 
 root.created = function () {
   this.$store.commit('changeMobileHeaderTitle', '我的推荐');
-  let protocol = window.location.protocol;
-  this.shareUrl = protocol + '//' + document.location.host + '/index/register?uid=' + this.$store.state.authMessage.userId + '&source=share';
 
-  this.shareConfig = {
-    url: this.shareUrl, // 网址，默认使用 window.location.href
-    source: '', // 来源（QQ空间会用到）
-    title: '二零二零', // 标题，默认读取 document.title 或者 <meta name="title" content="share.js" />
-    description: '二零二零', // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
-    image: 'http://logo.2020.exchange/website.jpg', // 图片, 默认取网页中第一个img标签
-    origin: window.location.origin,
-  }
+  let uuiid = this.$store.state.authMessage.userId;
+  let protocol = window.location.protocol;
+  let sharUrl = document.location.host + '/index/register?uid=' + uuiid + '&source=share';
+  this.shareUrl = protocol + '//' + sharUrl;
+
+  this.value = this.shareUrl;
+  // let protocol = window.location.protocol;
+  // this.shareUrl = protocol + '//' + document.location.host + '/index/register?uid=' + this.$store.state.authMessage.userId + '&source=share';
+
+  // this.shareConfig = {
+  //   url: this.shareUrl, // 网址，默认使用 window.location.href
+  //   source: '', // 来源（QQ空间会用到）
+  //   title: '二零二零', // 标题，默认读取 document.title 或者 <meta name="title" content="share.js" />
+  //   description: '二零二零', // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
+  //   image: 'http://logo.2020.exchange/website.jpg', // 图片, 默认取网页中第一个img标签
+  //   origin: window.location.origin,
+  // }
 
   this.getMyInvitesForBT()
-  this.value = this.shareUrl;
+
 
   // 获取邀请人数和获得奖励的请求
   // this.getUserRewardForInvites()
