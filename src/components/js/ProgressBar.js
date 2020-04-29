e2c4ed4d76d8cf760f88dfcc43102a5326bc1753
zuopengyu <zuopengyu2017@qq.com>
2020-04-29 18:07:40 +0800

    修改挂单后资产查询

--- src/components/js/ProgressBar.js
+++ src/components/js/ProgressBar.js
@@ -729,7 +729,12 @@ root.methods.Callback = function (data) {
   this.promptOpen = true;
   // 清空数量
   this.amount = '';
-  this.$http.send('ACCOUNTS', {bind: this, callBack: this.RE_ACCOUNTS})
+  // this.$http.send('ACCOUNTS', {bind: this, callBack: this.RE_ACCOUNTS})
+
+  setTimeout(()=>{
+    this.$http.send('ACCOUNTS', {bind: this, callBack: this.RE_ACCOUNTS})
+  },5000)
+
 }
 
 root.methods.RE_ERROR = function (err) {
