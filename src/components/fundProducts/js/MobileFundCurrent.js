4c422ca9c4ea65953247a9dd7b73efd65cdf7947
lcc <1398750946@qq.com>
2020-05-28 16:21:32 +0800

    添加详情页面

---
+++ src/components/fundProducts/js/MobileFundCurrent.js
@@ -0,0 +1,25 @@
+const root = {}
+root.name = 'mobileFundCurrent'
+/*------------------------------ 组件 ------------------------------*/
+//root.components = {
+//  'Loading': resolve => require(['../Loading/Loading.vue'], resolve),
+//}
+/*------------------------------ data -------------------------------*/
+root.data = function () {
+  return {}
+}
+/*------------------------------ 生命周期 -------------------------------*/
+root.created = function () {}
+root.mounted = function () {}
+root.beforeDestroy = function () {}
+/*------------------------------ 计算 -------------------------------*/
+root.computed = {}
+/*------------------------------ 观察 -------------------------------*/
+root.watch = {}
+/*------------------------------ 方法 -------------------------------*/
+root.methods = {}
+
+root.methods.returnFundDetails = function () {
+  this.$router.push({name:'mobileFundDetails'})
+}
+export default root
