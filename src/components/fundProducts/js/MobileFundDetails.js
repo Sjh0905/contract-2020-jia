a648d111f707008eb730e960a8def120d894ec05
zuopengyu <zuopengyu2017@qq.com>
2020-05-28 19:19:41 +0800

    申购详情和本期申购APP样式兼容

--- src/components/fundProducts/js/MobileFundDetails.js
+++ src/components/fundProducts/js/MobileFundDetails.js
@@ -9,11 +9,29 @@ root.data = function () {
   return {}
 }
 /*------------------------------ 生命周期 -------------------------------*/
-root.created = function () {}
+root.created = function () {
+  if(this.$route.query.isApp) {
+    window.postMessage(JSON.stringify({
+        method: 'setTitle',
+        parameters: '详情'
+      })
+    );
+    window.postMessage(JSON.stringify({
+      method: 'setH5Back',
+      parameters: {
+        canGoH5Back:true
+      }
+    }))
+  }
+}
 root.mounted = function () {}
 root.beforeDestroy = function () {}
 /*------------------------------ 计算 -------------------------------*/
 root.computed = {}
+// 检验是否是APP
+root.computed.isApp = function () {
+  return this.$route.query.isApp ? true : false
+}
 /*------------------------------ 观察 -------------------------------*/
 root.watch = {}
 /*------------------------------ 方法 -------------------------------*/
