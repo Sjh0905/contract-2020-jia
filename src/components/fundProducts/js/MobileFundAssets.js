06875354e39512d22dc626202ddba17f159028fa
lcc <1398750946@qq.com>
2020-06-02 18:05:16 +0800

    区块恋资产bug修复

--- src/components/fundProducts/js/MobileFundAssets.js
+++ src/components/fundProducts/js/MobileFundAssets.js
@@ -17,7 +17,7 @@ root.data = function () {
     totalBalance:0, // 总资产
     newQuantity:0, // 累计总收益
     last:0, // 最新收益
-    // loading:true
+    loading:true
   }
 }
 /*------------------------------ 生命周期 -------------------------------*/
@@ -49,17 +49,17 @@ root.methods.gotoDetails = function (type,item) {
 root.methods.getAssetStatus = function (type) {
   this.selectedType = type
   // 申购中
-  if(type === 1){
+  if(type == 1){
     this.getPurchase()
     return
   }
   // 收益中
-  if(type === 2){
+  if(type == 2){
     this.getIncome()
     return
   }
   // 已结束
-  if(type === 3){
+  if(type == 3){
     this.getHasEnded()
     return
   }
@@ -80,11 +80,11 @@ root.methods.getPurchase = function () {
 root.methods.re_getPurchase = function (data) {
   typeof(data) == 'string' && (data = JSON.parse(data));
   if(!data)return
-  this.purchaseList = data.dataMap.list
+  this.purchaseList = data.dataMap.list || []
   this.totalBalance = data.dataMap.balance
   this.newQuantity = data.dataMap.quantity
   this.last = data.dataMap.last
-  // this.loading = false
+  this.loading = false
 
 }
 root.methods.error_getPurchase = function (err) {
@@ -105,9 +105,9 @@ root.methods.getIncome = function (){
 root.methods.re_getIncome = function (data){
   typeof(data) == 'string' && (data = JSON.parse(data))
   if(!data)return
-  this.incomeList = data.dataMap.list
+  this.incomeList = data.dataMap.list ||[]
   this.totalBalance = data.dataMap.balance
-  // this.loading = false
+  this.loading = false
 }
 root.methods.error_getIncome = function (err){
   console.warn('err====',err)
@@ -127,8 +127,8 @@ root.methods.getHasEnded = function () {
 root.methods.re_getHasEnded = function (data){
   typeof (data) == 'string' && (data= JSON.parse(data))
   if(!data)return
-  this.hasEndedList = data.dataMap.list
-  // this.loading = false
+  this.hasEndedList = data.dataMap.list || []
+  this.loading = false
 }
 root.methods.error_getHasEnded = function (err) {
 
