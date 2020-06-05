f6e5e48a5e33e65ecd667c213c5f3af6d7979e19
sunjiahui <2570167180@qq.com>
2020-06-05 15:28:08 +0800

    H5 热度添加loading

--- src/components/js/MobileHeatList.js
+++ src/components/js/MobileHeatList.js
@@ -3,12 +3,12 @@ const root = {};
 root.name = 'MobileHeatList';
 
 root.components = {
-  // 'Loading': resolve => require(['../../vue/Loading'], resolve),
+  'Loading': resolve => require(['../vue/Loading'], resolve),
 }
 
 root.data = function () {
   return {
-    loading: false,
+    loading: true,
     heatList: [],
     isApp:false,
     isIOS:false
@@ -53,6 +53,7 @@ root.methods.re_getHeatList = function (data) {
   typeof(data) == 'string' && (data = JSON.parse(data));
   console.info('data======sssss',data)
   this.heatList = data.data
+  this.loading = false
 }
 
 // 判断是否是ios打开
