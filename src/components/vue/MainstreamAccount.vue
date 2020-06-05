caa77d57313af04ad3caabf971962a9bf79006a4
lcc <1398750946@qq.com>
2020-06-05 12:00:39 +0800

    添加主流账户路由

---
+++ src/components/vue/MainstreamAccount.vue
@@ -0,0 +1,4 @@
+<template src="../html/MainstreamAccount.html"></template>
+<style scoped src="../css/MainstreamAccount.css"></style>
+<script src="../js/MainstreamAccount.js"></script>
+<i18n src="../lang/MainstreamAccount.json"></i18n>
