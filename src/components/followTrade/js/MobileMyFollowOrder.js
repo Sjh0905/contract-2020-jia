57cee6fa157f03d35675b74ae5b623920f10c29e
zuopengyu <zuopengyu2017@qq.com>
2020-06-13 20:02:10 +0800

    增加我的跟单

---
+++ src/components/followTrade/js/MobileMyFollowOrder.js
@@ -0,0 +1,35 @@
+const root = {}
+root.name = 'mobileFollowTradeStrategy'
+/*------------------------------ 组件 ------------------------------*/
+//root.components = {
+//  'Loading': resolve => require(['../Loading/Loading.vue'], resolve),
+//}
+/*------------------------------ data -------------------------------*/
+root.data = function () {
+  return {
+    followType:1
+  }
+}
+/*------------------------------ 生命周期 -------------------------------*/
+root.created = function () {}
+root.mounted = function () {}
+root.beforeDestroy = function () {}
+/*------------------------------ 计算 -------------------------------*/
+root.computed = {}
+/*------------------------------ 观察 -------------------------------*/
+root.watch = {}
+/*------------------------------ 方法 -------------------------------*/
+root.methods = {}
+// 切换历史跟单和跟随者
+root.methods.toggleType = function (type) {
+  this.followType = type
+}
+// 返回跟单首页
+root.methods.jumpToFollowTrade = function () {
+  this.$router.push({name:'mobileFollowTrade'})
+}
+// 个人设置
+root.methods.personalSetting = function () {
+  console.info('personalSetting=======个人设置',)
+}
+export default root
