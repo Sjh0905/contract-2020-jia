ca938baf7cf331e1bfa68c22052591ec16f3362f
sunjiahui <2570167180@qq.com>
2020-06-17 14:23:23 +0800

    跟单首页处理冲突

--- src/components/followTrade/js/MobileTapeListManage.js
+++ src/components/followTrade/js/MobileTapeListManage.js
@@ -85,6 +85,11 @@ root.methods.re_postCommitFee = function (data) {
   console.log("this.res=====",data)
   typeof data === 'string' && (data = JSON.parse(data))
   console.info('data',data)
+  if(data.errorCode == 0) {
+    this.openMaskWindow = false
+    this.isTapeList = true
+    this.openPop('订阅成功',1)
+  }
 }
 root.methods.error_postCommitFee = function (err) {
   console.log("this.err=====",err)
