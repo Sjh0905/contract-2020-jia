6cdb99235e13c3b53ea0b73c3d70eda359f1e3fc
lcc <1398750946@qq.com>
2020-08-20 10:45:08 +0800

    修改打版内容

--- src/components/vue/OrderPosition.vue
+++ src/components/vue/OrderPosition.vue
@@ -1,4 +1,4 @@
 <template src="../html/OrderPosition.html"></template>
 <style scoped src="../css/OrderPosition.css"></style>
 <script src="../js/OrderPosition.js"></script>
-<i18n src="../lang/orderPosition.json"></i18n>
+<i18n src="../lang/OrderPosition.json"></i18n>
