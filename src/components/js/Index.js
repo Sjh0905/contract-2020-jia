016851f6fc9039839048973a78b97faf28924134
lcc <1398750946@qq.com>
2020-09-22 12:55:14 +0800

    处理合并bug

--- src/components/js/Index.js
+++ src/components/js/Index.js
@@ -291,7 +291,8 @@ root.methods.re_getLeverageBracket = function(data){
     // console.log('this is getLeverageBracket=',v.notionalCap,this.$globalFunc.accMinus(v.notionalCap,9223372036854776000));
   })
 
-  // bracketSingle = bracketSingle.reverse();//必须按bracket倒序排序，其他代码是这么用的
+  bracketSingle = bracketSingle.sort((a,b)=>{return (a.bracket -b.bracket)});//必须按bracket升序排序，其他代码是这么用的
+  console.info('bracketSingle==',bracketSingle)
 
   this.$store.commit('CHANGE_LEVERAGE_BRACKET',bracketSingle);
   // console.info('this is getLeverageBracket=',item,bracketSingle)
