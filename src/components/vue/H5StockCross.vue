a099e7649cf51b855d7cac5071ee20cd825496ca
zuopengyu <zuopengyu2017@qq.com>
2020-10-14 17:20:37 +0800

    添加H5盘口文件

---
+++ src/components/vue/H5StockCross.vue
@@ -0,0 +1,4 @@
+<template src="../html/H5StockCross.html"></template>
+<style scoped src="../css/H5StockCross.css"></style>
+<script src="../js/H5StockCross.js"></script>
+<i18n src="../lang/H5StockCross.json"></i18n>
