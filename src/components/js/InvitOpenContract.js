31da4886dff9ec0ea0b629bd81320eb35b751ce6
lcc <1398750946@qq.com>
2020-11-19 20:09:16 +0800

    邀请海报添加跳转

---
+++ src/components/js/InvitOpenContract.js
@@ -0,0 +1,73 @@
+const root = {}
+root.name = 'InvitOpenContract'
+/*------------------------------ 组件 ------------------------------*/
+root.components = {
+//  'Loading': resolve => require(['../Loading/Loading.vue'], resolve),
+}
+/*------------------------------ data -------------------------------*/
+root.data = function () {
+  return {
+    invitCodeInput:'',
+    isSelect: 1,
+    name_0:'',
+  }
+}
+/*------------------------------ 生命周期 -------------------------------*/
+root.created = function () {
+  if (this.$route.query.uid) {
+    this.invitCodeInput = this.$route.query.uid
+  }
+}
+root.mounted = function () {}
+root.beforeDestroy = function () {}
+/*------------------------------ 计算 -------------------------------*/
+root.computed = {}
+// 判断是否是手机
+root.computed.isMobile = function () {
+  return this.$store.state.isMobile
+}
+// 用户id，判断是否登录
+root.computed.userId = function () {
+  return this.$store.state.authState.userId
+}
+/*------------------------------ 观察 -------------------------------*/
+root.watch = {}
+/*------------------------------ 方法 -------------------------------*/
+root.methods = {}
+// 登录并开通
+root.methods.goToLogin = function () {
+  if(this.isSelect) return
+  if(!this.$globalFunc.testNumber(this.invitCodeInput)){
+    this.name_0 = '请输入正确的邀请码'
+    return
+  }
+  if(!this.invitCodeInput){
+    this.name_0 = '请输入邀请码'
+    return
+  }
+  window.location.replace(this.$store.state.contract_url + 'index/sign/login?type=contract&uid='+this.$route.query.uid);
+  // console.info('登录并开通')
+}
+// 注册并开通
+root.methods.goToRegister = function () {
+  if(this.isSelect) return
+  if(!this.$globalFunc.testNumber(this.invitCodeInput)){
+    this.name_0 = '请输入正确的邀请码'
+    return
+  }
+  if(!this.invitCodeInput){
+    this.name_0 = '请输入邀请码'
+    return
+  }
+  window.location.replace(this.$store.state.contract_url + 'index/register?type=contract&uid='+this.$route.query.uid);
+  // console.info('注册并开通')
+}
+// 改变输入框的选择状态
+root.methods.changeSelect = function (type) {
+  this.isSelect = type
+}
+// 关闭后跳转首页
+root.methods.contractOpen = function () {
+  window.location.replace(this.$store.state.contract_url + 'index/newH5homePage');
+}
+export default root
---
+++ src/components/vue/InvitOpenContract.vue
@@ -0,0 +1,3 @@
+<template src="../html/InvitOpenContract.html"></template>
+<style scoped src="../css/InvitOpenContract.css"></style>
+<script src="../js/InvitOpenContract.js"></script>
