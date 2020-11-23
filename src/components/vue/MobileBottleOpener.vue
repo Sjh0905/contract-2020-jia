c68b24820178e27dc847a5c479d8b29d7376b6c4
lcc <1398750946@qq.com>
2020-11-23 18:54:47 +0800

    开平器添加可开数量 + h5开平器路由组件配置

---
+++ src/components/vue/MobileBottleOpener.vue
@@ -0,0 +1,5 @@
+<template src="../html/MobileBottleOpener.html"></template>
+<style scoped src="../css/MobileBottleOpener.css"></style>
+<!--JS 公用一个组件-->
+<!--<script src="../js/MobileBottleOpener.js"></script>-->
+<script src="../js/KaipingqiPopWindow.js"></script>
