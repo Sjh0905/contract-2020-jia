2f7439f82ba1a63a20a3c3dec5c241ebe8b4dc05
lcc <1398750946@qq.com>
2020-12-15 19:50:34 +0800

    屏蔽H5标记价格接口传参

--- src/components/js/MobileTradingHallDetail.js
+++ src/components/js/MobileTradingHallDetail.js
@@ -1926,9 +1926,9 @@ root.methods.setTotalAmountShort = function(totalAmountShort){
 root.methods.getMarkPricesAndCapitalRates = function () {
   this.$http.send('GET_MARKET_PRICE',{
     bind: this,
-    query:{
-      symbol:this.capitalSymbol
-    },
+    // query:{
+    //   symbol:this.capitalSymbol
+    // },
     callBack: this.re_getMarkPricesAndCapitalRates,
     errorHandler:this.error_getMarkPricesAndCapitalRates
   })
